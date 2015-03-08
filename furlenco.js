43af06d80791102e4b5b27ba735e99438eca54ea
Vividh <vividh@fusioncharts.com>
2015-03-09 02:57:20 +0530

    first commit

---
+++ furlenco.js
@@ -0,0 +1,35 @@
+var FURLENCO = {
+	'ranges': [{
+	    'name': 'Pico Novo',
+	    'description': '',
+	    'cover': ''
+
+	}, {
+	    'name': 'Pico Classic',
+	    'description': '',
+	    'cover': ''
+	}],
+	'rooms': [{
+	    'configurable': true,
+	    'name': 'Bedroom',
+	    'description': '',
+	    'furnishings': [
+	        'couch'
+	    ]
+	}, {
+	    'type': 'living',
+	    'name': 'Living Room',
+	    'description': ''
+	}, {
+	    'type': 'dining',
+	    'name': 'Dining Room',
+	    'description': ''
+	}],
+	'themes': [{
+		'name': 'Rainforest',
+		'description': ''
+	}, {
+		'name': 'Sun and Sand',
+		'description': ''
+	}]
+};
